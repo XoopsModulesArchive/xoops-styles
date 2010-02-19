9f1b0f150a0a311a7ef821085a073feb8a615a85
kris_fr <kris_fr@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-02-19 16:13:06 +0000

    Add Xoops-Styles Module maintained by Xoops Design Team (thank's burning)

---
+++ xoops_version.php
@@ -0,0 +1,15 @@
+<?php
+$modversion['name'] = "xoops-styles";
+$modversion['version'] = 1;
+$modversion['description'] = "Faux module Xoops Documentation";
+$modversion['credits'] = "Chewb - Christian - Dugris - Kris_fr";
+$modversion['author'] = "Burning";
+$modversion['help'] = "";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 1;
+$modversion['image'] = "docs_slogo.png";
+$modversion['dirname'] = "xoops-styles";
+
+// Menu
+$modversion['hasMain'] = 1;
+?>
